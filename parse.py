4468eb30ff2a398e4c7fd4d7f146e1e20e438df3
Cameron Ryan <cjryanwashere@gmail.com>
2023-05-17 13:41:19 -0400

    improving training notebook

--- parse.py
+++ parse.py
@@ -26,9 +26,34 @@ def page_info(url):
 
                 print(image_url)
 
+def filter_tag(tag) -> bool:
+
+    # get rid of images that are too small to
+    # contain content
+    if 'width' in tag.attrs:
+        if int(tag['width']) < 50:
+            return False
+    if 'height' in tag.attrs:
+        if int(tag['height']) < 50:
+            return False 
+    
+    # we assume that it already has a source
+    image_url = tag['src']
+    if 'static' in image_url:
+        return False
+    if 'wikipedia' in image_url and 'logo' in image_url:
+        return False
+        
+
+    return True
+
 def process_page(url, root_path):
 
+    page_name = url.split("/")[-1]
+
     output_str = ""
+
+    image_count = 0
 
     response = requests.get(url)
     if response.status_code == 200:
@@ -39,8 +64,9 @@ def process_page(url, root_path):
             if 'src' in img_tag.attrs:
                     image_url = img_tag['src']
 
-                    if "static" in image_url:
+                    if not filter_tag(img_tag):
                         continue
+
 
                     if not "https://" in image_url:
                         image_url = urljoin(url, image_url)  # Handle relative URLs
@@ -49,17 +75,19 @@ def process_page(url, root_path):
                     image_response = requests.get(image_url)
                     if image_response.status_code == 200:
 
-                        #image_name = os.path.basename(image_url)
-                        #image_uuid = image_name
-                        image_uuid = str(uuid.uuid4())
+                        image_name = os.path.basename(image_url)
+                        image_uuid = image_name
+                        #image_uuid = str(uuid.uuid4())
 
                         try:
                             if not image_url.endswith(".svg"):   
                                 image = Image.open(BytesIO(image_response.content))
-                                output_path_jpg = f"./{root_path}/images/{image_uuid}.jpg"
+                                output_path_jpg = f"{root_path}/images/{image_uuid}.jpg"
                                 convert_and_resize_image(image, output_path_jpg, (224, 224))
                                 output_str += f"{output_path_jpg}\n"
+                                image_count += 1
                         except:
+                            #print("failed to save image")
                             pass
                     else:
                         pass
@@ -76,13 +104,14 @@ def process_page(url, root_path):
 
 
         
-        page_uuid = str(uuid.uuid4())
+        #page_uuid = str(uuid.uuid4())
+        page_uuid = page_name
 
-        path = f"./{root_path}/pages/{page_uuid}.txt"
+        path = f"{root_path}/pages/{page_uuid}.txt"
 
         with open(path, "w") as f:
             f.write(output_str)
-        return f"saved web page data: {path}"
+        return f"saved web page data: {path} with {image_count} images"
 
     else:
         return f'Failed to fetch the web page: {response.status_code}'
